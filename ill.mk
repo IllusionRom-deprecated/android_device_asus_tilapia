93bb9e640d7f7efaec1c81f9c1a440bf5e6d3bee
Snuzzo <jrracinfan@gmail.com>
2014-04-21 12:30:16 -0400

    Illusionize

--- ill.mk
+++ ill.mk
@@ -5,24 +5,21 @@ PRODUCT_RELEASE_NAME := Nexus7-GSM
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 800
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-
-# Inherit telephony common stuff
-$(call inherit-product, vendor/cm/config/telephony.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/illusion/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/tilapia/full_tilapia.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tilapia
-PRODUCT_NAME := cm_tilapia
+PRODUCT_NAME := ill_tilapia
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasig BUILD_FINGERPRINT="google/nakasig/tilapia:4.4/KRT16S/920375:user/release-keys" PRIVATE_BUILD_DESC="nakasig-user 4.4 KRT16S 920375 release-keys"
+
+# bootanimation
+PRODUCT_COPY_FILES += \
+        vendor/illusion/bootanimation/Illusion768x768.zip:system/media/bootanimation.zip
